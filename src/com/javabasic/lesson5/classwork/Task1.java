adc5f67066fc5f507af13aba85b15787c5392193
andry80 <8888appl8888@gmail.com>
2023-12-08 16:36:51 +0200

    lesson5 classwork

---
+++ src/com/javabasic/lesson5/classwork/Task1.java
@@ -0,0 +1,24 @@
+package com.javabasic.lesson5.classwork;
+
+public class Task1 {
+    public static void main(String[] args) {
+        double lenght = 1.5;
+        double width = 0.5;
+        double height = 1;
+
+        double volume  = calculateVolume(lenght, width, height);
+        System.out.println(volume);
+
+        double totalWeight = calculateTotalWeight(volume);
+        System.out.println(totalWeight);
+    }
+
+    public static double calculateVolume(double l, double w, double h) {
+        return l * w * h;
+    }
+
+    public static double calculateTotalWeight(double volume) {
+        double maxWeight = 58;
+        return volume * maxWeight;
+    }
+}
