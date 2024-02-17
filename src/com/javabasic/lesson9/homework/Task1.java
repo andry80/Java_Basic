f04ffbed1f1d07aa2e400152d26fc9a406a1aebb
andry80 <8888appl8888@gmail.com>
2024-02-17 15:34:12 +0200

    lesson9 homework

---
+++ src/com/javabasic/lesson9/homework/Task1.java
@@ -0,0 +1,69 @@
+package com.javabasic.lesson9.homework;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task1 {
+    static int[] list1 = new int[7];
+    static int[] list2 = new int[7];
+
+    public static void main(String[] args) {
+        random(list1);
+        System.out.println(Arrays.toString(list1));
+        mySort(list1);
+        System.out.println(Arrays.toString(list1));
+
+        inputNum(list2);
+        System.out.println(Arrays.toString(list2));
+        mySort(list2);
+        System.out.println(Arrays.toString(list2));
+
+        printCoincidence(list1, list2);
+    }
+
+    public static void inputNum(int[] list) {
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        while (list.length > i) {
+            System.out.print("Введите число (от 0 до 9): ");
+            int num = scanner.nextInt();
+            if (num <= 9 && num >=0) {
+                list[i] = num;
+                i++;
+            }else {
+                continue;
+            }
+
+        }
+    }
+
+     public static void random(int[] list) {
+         for (int i = 0; i < list.length; i++) {
+//             list[i] = (int) (Math.random() * (10  - 0) + 0);
+             list[i] = (int) (Math.random() * 10);
+         }
+     }
+
+     public static void  mySort(int[] list) {
+        for (int i = 0; i< list.length; i++) {
+            for (int j = 0; j < list.length-1-i; j++) {
+                if (list[j] > list[j+1]) {
+                    int u = list[j+1];
+                    list[j+1] = list[j];
+                    list[j] = u;
+                }
+            }
+        }
+     }
+
+     public static void printCoincidence(int[] list1, int[] list2) {
+        int t = 0;
+        for (int i = 0; i < list1.length; i++) {
+            if (list1[i]==list2[i]) {
+                t++;
+            }
+        }
+         System.out.println(t);
+     }
+}
---
+++ src/com/javabasic/lesson9/homework/Task2.java
@@ -0,0 +1,37 @@
+package com.javabasic.lesson9.homework;
+
+import java.util.Arrays;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int[] list = new int[10];
+        random(list);
+        System.out.println(Arrays.toString(list));
+
+        mySort(list);
+
+    }
+
+    public static void random(int[] list) {
+        for (int i = 0; i < list.length; i++) {
+            list[i] = (int) (Math.random() * 10);
+        }
+    }
+
+    public static void mySort(int[] list) {
+        int y = 0;
+        for (int i = 0; i < list.length; i++) {
+            for (int j = i; j < list.length; j++) {
+                y++;
+                if (list[j] == 0) {
+                    int u = list[j];
+                    list[j] = list[i];
+                    list[i] = u;
+                    break;
+                }
+            }
+        }
+        System.out.println(y);
+        System.out.println(Arrays.toString(list));
+    }
+}
